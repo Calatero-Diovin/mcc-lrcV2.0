94f36d145d955278aad2186689cee734b102f465
Black Mann <caranzocrisa@gmail.com>
2024-09-29 18:33:39 +0800

    commit

---
+++ portal.php
@@ -0,0 +1,16 @@
+<?php
+// Get the 'portal' query parameter
+$portal = isset($_GET['portal']) ? $_GET['portal'] : '.'; // Default to 'home'
+
+// Define a list of valid pages
+$validPages = ['.', 'about', 'contact'];
+
+// Check if the requested page is valid
+if (in_array($portal, $validPages)) {
+    // Include the corresponding PHP file
+    include $portal . '.php';
+} else {
+    // If the page is not valid, show a 404 error
+    include '404.php'; // Create a simple 404.php file for not found pages
+}
+?>
