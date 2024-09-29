8bd48720df8b33c9d0f408f0e8e68f8fa132a509
Black Mann <caranzocrisa@gmail.com>
2024-09-29 18:46:02 +0800

    commit

--- includes/navbar.php
+++ includes/navbar.php
@@ -38,8 +38,8 @@ include('admin/config/dbcon.php');
                               href=".">Homes</a>
                     </li>
                     <li class="nav-item">
-                         <a class="nav-link  text-white <?=$page == 'about.php' ? 'active': '' ?> fw-semibold"
-                              href="about.php">About</a>
+                         <a class="nav-link  text-white <?=$page == 'about' ? 'active': '' ?> fw-semibold"
+                              href="about">About</a>
                     </li>
                     <li class="nav-item">
                          <a class="nav-link  text-white <?=$page == 'services' ? 'active': '' ?> fw-semibold"
