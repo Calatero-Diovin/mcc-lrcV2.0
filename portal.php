9986a4c3c075b48def535af638963c15b1fa0d94
Black Mann <caranzocrisa@gmail.com>
2024-09-29 18:37:08 +0800

    commit

--- portal.php
+++ portal.php
@@ -8,7 +8,7 @@ $validPages = ['.', 'about', 'contact'];
 // Check if the requested page is valid
 if (in_array($portal, $validPages)) {
     // Include the corresponding PHP file
-    include $portal . '.php';
+    include $portal;
 } else {
     // If the page is not valid, show a 404 error
     include '404.php'; // Create a simple 404.php file for not found pages
