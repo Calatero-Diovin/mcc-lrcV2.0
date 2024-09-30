2a1de3895fad21989e99bc63b5ce0d59b989273e
Black Mann <caranzocrisa@gmail.com>
2024-09-30 19:02:48 +0800

    commit

--- admin/authentication.php
+++ admin/authentication.php
@@ -5,7 +5,7 @@ include('config/dbcon.php');
 if(!isset($_SESSION['auth']))
 {
   $_SESSION['message_error'] = "Login to Access Dashboard";
-  header("Location:admin_login");
+  header("Location:../admin_login");
   exit(0);
 }
 else
@@ -13,7 +13,7 @@ else
   if($_SESSION['auth_role'] != "Admin" && $_SESSION['auth_role'] != "Staff")
   {
     $_SESSION['message_error'] = "<small>You are not authorized to access this page</small>";
-    header("Location:admin_login");
+    header("Location:../admin_login");
     exit(0);
   }
 }
