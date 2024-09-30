d7a6444392e89cbade4c420dce3a8ef2a220f1a0
Black Mann <caranzocrisa@gmail.com>
2024-09-30 19:17:08 +0800

    commit

--- home.php
+++ home.php
@@ -47,7 +47,37 @@ include('includes/navbar.php');
     </div>
 </div>
 
+<!-- Footer -->
+<div class="jumbotron">
+    <footer class="text-center text-lg-start text-white" style="background-color: #0D4C92">
+        <!-- Grid container -->
+        <div class="jumbotron p-4 pb-0">
 
+            <hr class="my-3">
+
+            <section class="p-3 pt-0">
+                <div class="row d-flex align-items-center">
+                    <!-- Grid column -->
+                    <div class="col-md-7 col-lg-8 text-center text-md-start">
+                        <div class="p-3">
+                            Madridejos Community College 2.0
+                        </div>
+                    </div>
+                    <!-- Grid column -->
+
+                    <!-- Grid column -->
+                    <div class="col-md-5 col-lg-4 ml-lg-0 text-center text-md-end">
+                        <a href="https://www.facebook.com/madridejoscommunitycollege" class="btn btn-outline-light btn-floating m-1" role="button"><i class="bi bi-facebook"></i></a>
+                        <a href="https://www.youtube.com/watch?v=bIzChSbj0OU" class="btn btn-outline-light btn-floating m-1" role="button"><i class="bi bi-youtube"></i></a>
+                    </div>
+                    <!-- Grid column -->
+                </div>
+            </section>
+        </div>
+        <!-- Grid container -->
+    </footer>
+</div>
+<!-- End of .container -->
 
 <?php 
 include('includes/script.php');
