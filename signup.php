f97d14bcc36f61280597693cb8d5a82e2171e040
Black Mann <caranzocrisa@gmail.com>
2024-09-30 17:18:18 +0800

    commit

--- signup.php
+++ signup.php
@@ -244,7 +244,7 @@ $code_row = $code_result->fetch_assoc();
 
                     <div class="field">
                         <div class="label">
-                            <input type="checkbox" id="termsCheckbox" style="margin-left:-40px;" required>
+                            <input type="checkbox" id="termsCheckbox" style="margin-left:-40px;margin-right:-20px;" required>
                             <label for="termsCheckbox">I agree to the</label>
                         </div>
                     </div>
