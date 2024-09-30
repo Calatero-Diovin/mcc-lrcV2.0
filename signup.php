1aeebf72c0afd0e5362a25d573500403c4af4114
Black Mann <caranzocrisa@gmail.com>
2024-09-30 17:29:16 +0800

    commit

--- signup.php
+++ signup.php
@@ -242,7 +242,7 @@ $code_row = $code_result->fetch_assoc();
                         <input type="text" name="middlename" id="middlename" />
                     </div>
 
-                    <div class="form-check" style="margin-top:-30px;">
+                    <div class="form-check" style="margin-left:10px;margin-top:-30px;">
                         <input type="checkbox" class="form-check-input" id="exampleCheck1">
                         <label class="form-check-label" for="exampleCheck1">I agree to the <a href="#" target="_blank">Terms and Conditions</a></label>
                     </div>
