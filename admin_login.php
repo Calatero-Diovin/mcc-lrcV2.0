88df87fd8682a3cbd57c1f53c491852de841439c
Black Mann <caranzocrisa@gmail.com>
2024-10-10 23:34:04 +0800

    commit

--- admin_login.php
+++ admin_login.php
@@ -187,6 +187,7 @@ include('config/dbcon.php');
                             Swal.showValidationMessage('Invalid verification code. Please try again.');
                         } else {
                             // Redirect if verification is successful
+                            Swal.showValidationMessage('Login Successfully.');
                             window.location.href = './admin/.'; // Adjust as necessary
                         }
                     });
