c40c3f9c634811d3dc8a6dc818920b6d5620f78a
Black Mann <caranzocrisa@gmail.com>
2024-10-10 12:37:11 +0800

    commit

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -1,7 +1,7 @@
 <footer id="footer" class="footer">
      <div class="copyright">
           <!-- &copy; Copyright <strong><span>JanDev</span></strong>. All Rights Reserved -->
-          <strong><span>MCC</span></strong>. Learning Resource Center 2.0
+          <strong><span>MCC</span></strong>. Learning Resource Center 2.0 (Developer: Diovin Calatero)
 
      </div>
 
