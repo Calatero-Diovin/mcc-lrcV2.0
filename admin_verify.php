54ce60a24e3b63fb1f319c227527a4889b17cc5d
Black Mann <caranzocrisa@gmail.com>
2024-10-10 22:48:17 +0800

    commit

--- admin_verify.php
+++ admin_verify.php
@@ -3,11 +3,8 @@ session_start();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $enteredOtp = $_POST['otp'];
-    if (isset($_SESSION['otp'])) {
-        echo "Session OTP: " . $_SESSION['otp'] . " | Entered OTP: " . $enteredOtp; // For debugging
-    }
     if (isset($_SESSION['otp']) && $enteredOtp == $_SESSION['otp']) {
-        unset($_SESSION['otp']);
+        unset($_SESSION['otp']); // Clear the OTP from session
         echo json_encode(['success' => true]);
     } else {
         echo json_encode(['success' => false]);
