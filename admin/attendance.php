410785c802c199bdb8968f9dbf505291eb1903dd
Black Mann <caranzocrisa@gmail.com>
2024-10-12 23:03:36 +0800

    commit

--- admin/attendance.php
+++ admin/attendance.php
@@ -4,6 +4,9 @@ include('includes/header.php');
 include('./includes/sidebar.php'); 
 ?>
 
+<link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.dataTables.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/buttons/3.1.2/css/buttons.dataTables.css">
+
 <main id="main" class="main">
      <div class="pagetitle" data-aos="fade-down">
 
@@ -138,6 +141,16 @@ include('./includes/sidebar.php');
           </div>
      </section>
 </main>
+
+<script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+<script src="https://cdn.datatables.net/2.1.8/js/dataTables.js"></script>
+<script src="https://cdn.datatables.net/buttons/3.1.2/js/dataTables.buttons.js"></script>
+<script src="https://cdn.datatables.net/buttons/3.1.2/js/buttons.dataTables.js"></script>
+<script src="https://cdn.datatables.net/buttons/3.1.2/js/buttons.print.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/3.1.2/js/buttons.html5.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
 
 <script>
      new DataTable('#example', {
