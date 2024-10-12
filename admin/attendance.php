69df66ac8e78072a7adc3ae79a0be18aa6bc82a2
Black Mann <caranzocrisa@gmail.com>
2024-10-12 23:06:55 +0800

    commit

--- admin/attendance.php
+++ admin/attendance.php
@@ -89,14 +89,11 @@ include('./includes/sidebar.php');
                                                           <tr>
                                                                <?php date_default_timezone_set('Asia/Manila'); ?>
                                                                
-                                                               <td><?= $row['firstname'].' '.$row['middlename'].' '.$row['lastname']; ?>
-                                                               </td>
-                                                               <td><?= date("h:i a", strtotime($row['time_log'])); ?>
-                                                               </td>
-                                                               <td><?= date("M d, Y", strtotime($row['date_log'])); ?>
-                                                               </td>
-                                                               <td><?=$row['year_level'].' - '.$row['course']; ?></td>
-                                                               <td><?= date("h:i a", strtotime($row['time_out'])); ?></td>
+                                                            <td><?= date("M d, Y", strtotime($row['date_log'])); ?></td>
+                                                            <td><?= date("h:i a", strtotime($row['time_log'])); ?></td>
+                                                            <td><?= $row['firstname'].' '.$row['middlename'].' '.$row['lastname']; ?></td>
+                                                            <td><?=$row['year_level'].' - '.$row['course']; ?></td>
+                                                            <td><?= date("h:i a", strtotime($row['time_out'])); ?></td>
                                                           </tr>
                                                           <?php      }
                             }
@@ -113,12 +110,12 @@ include('./includes/sidebar.php');
                                                                                      <?php date_default_timezone_set('Asia/Manila'); ?>
                                                                                      <td><?= date("M d, Y", strtotime($row['date_log'])); ?>
                                                                                      </td>
-                                                                                     <td><?= date("h:i:s a", strtotime($row['time_log'])); ?>
+                                                                                     <td><?= date("h:i a", strtotime($row['time_log'])); ?>
                                                                                      </td>
                                                                                      <td><?= $row['firstname'].' '.$row['middlename'].' '.$row['lastname']; ?>
                                                                                      </td>
                                                                                      <td><?=$row['year_level'].' - '.$row['course']; ?></td>
-                                                                                     <td><?= date("h:i:s a", strtotime($row['time_out'])); ?></td>
+                                                                                     <td><?= date("h:i a", strtotime($row['time_out'])); ?></td>
                                                                                 </tr>
                                                                       <?php } 
                                                        }
