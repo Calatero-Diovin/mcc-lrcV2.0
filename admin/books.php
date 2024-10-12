8de9cf8abb08f93b179ca650ec5ae6afe77e05dc
Black Mann <caranzocrisa@gmail.com>
2024-10-12 23:41:41 +0800

    commit

--- admin/books.php
+++ admin/books.php
@@ -4,6 +4,9 @@ include('includes/header.php');
 include('./includes/sidebar.php'); 
 ?>
 
+<link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.dataTables.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/buttons/3.1.2/css/buttons.dataTables.css">
+
 <style>
     #hover:hover {
         text-decoration: underline;
@@ -208,6 +211,16 @@ include('./includes/footer.php');
 include('./includes/script.php');
 include('../message.php');   
 ?>
+
+<script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+<script src="https://cdn.datatables.net/2.1.8/js/dataTables.js"></script>
+<script src="https://cdn.datatables.net/buttons/3.1.2/js/dataTables.buttons.js"></script>
+<script src="https://cdn.datatables.net/buttons/3.1.2/js/buttons.dataTables.js"></script>
+<script src="https://cdn.datatables.net/buttons/3.1.2/js/buttons.print.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/3.1.2/js/buttons.html5.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
 
 <script>
 document.addEventListener('DOMContentLoaded', function () {
