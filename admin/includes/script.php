4b976d8d594a6e5992e2d8e1bc049549191dcf18
Black Mann <caranzocrisa@gmail.com>
2024-10-16 21:40:40 +0800

    commit

--- admin/includes/script.php
+++ admin/includes/script.php
@@ -30,7 +30,6 @@
 <script src="assets/js/sweetalert2.all.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.0/dist/sweetalert2.all.min.js"></script>
 
-<script src="https://code.jquery.com/jquery-3.7.1.js"></script>
 <script src="https://cdn.datatables.net/2.1.8/js/dataTables.js"></script>
 <script src="https://cdn.datatables.net/rowreorder/1.5.0/js/dataTables.rowReorder.js"></script>
 <script src="https://cdn.datatables.net/rowreorder/1.5.0/js/rowReorder.dataTables.js"></script>
