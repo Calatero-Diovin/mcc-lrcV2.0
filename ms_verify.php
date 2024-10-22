3af53732028c821e3b576ccc0a69cce9330309bd
Black Mann <caranzocrisa@gmail.com>
2024-10-22 18:22:11 +0800

    commit

--- ms_verify.php
+++ ms_verify.php
@@ -1,7 +1,6 @@
-<?php
+<?php 
 ini_set('session.cookie_httponly', 1);
 session_start();
-include('./admin/config/dbcon.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,11 +9,11 @@ include('./admin/config/dbcon.php');
      <meta charset="UTF-8">
      <meta http-equiv="X-UA-Compatible" content="IE=edge">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <link rel="icon" href="./images/mcc-lrc.png">
+     <link rel="icon" href="images/mcc-lrc.png">
      <title>MS 365 Verification</title>
-
-     <!-- Alertify JS link -->
-     <link rel="stylesheet" href="assets/css/alertify.min.css" />
+     
+        <!-- Alertify JS link -->
+        <link rel="stylesheet" href="assets/css/alertify.min.css" />
      <link rel="stylesheet" href="assets/css/alertify.bootstraptheme.min.css" />
      <link rel="stylesheet" href="assets/css/bootstrap-icons.min.css">
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.8.1/font/bootstrap-icons.min.css">
@@ -33,11 +32,14 @@ include('./admin/config/dbcon.php');
      <link rel="stylesheet" href="assets/css/login.css">
 
      <style>
-          .back {
+          .back{
+               position: fixed;
+               left:20px;
+               top:10px;
                font-size: 30px;
                color: black;
           }
-          .back:hover {
+          .back:hover{
                color: gray;
           }
      </style>
@@ -48,34 +50,42 @@ include('./admin/config/dbcon.php');
           <div class="container-xl">
                <div class="col mx-auto rounded shadow bg-white">
                     <div class="row">
-                    <a href="login" class="back">
-                              <i class="bi bi-arrow-left-circle-fill m-3"></i>
-                         </a>
                          <div class="col-md-6 ">
                               <div class="">
                                    <img src="images/mcc-lrc.png" alt="logo"
                                         class="img-fluid d-none d-md-block  p-5" />
                               </div>
                          </div>
-                         <div class="col-sm-12 col-md-6 px-5 ">
-                              <div class="mb-4">
+                         <div class="col-sm-12 col-md-6 px-5 " style="margin-top: 60px;">
+                              <div class="mt-3 mb-4">
                                    <center>
-                                        <h1 class="m-0"><strong>MS 365 Account Verification</strong></h1>
-                                        <p class="fs-4 fw-semibold text-info">Enter your MS 365 Username account to receive a registration link.</p>
+                                        <h2 class="m-0 fw-semibold text-primary">
+                                             MS 365 Account Verification
+                                        </h4>
+                                        <br>
+                                        <p class="fs-5 fw-semibold">Enter your MS 365 Username account to receive a registration link.</p>
                                    </center>
                               </div>
-                              <form action="ms_verify_code.php" method="POST" class="needs-validation" novalidate>
+                              <form action="ms_verify_code.php" method="POST" class="needs-validation" novalidate
+                              style="margin-top:30px;">
                                    <div class="col-md-12">
                                         <div class="form-floating mb-3">
-                                             <input type="email" id="email" class="form-control" name="email" placeholder="MS 365 Email" required>
-                                             <label id="student_id_label" for="student_id">Student ID No.</label>
+                                             <input type="email" id="email" class="form-control"
+                                                  name="email" placeholder="MS 365 Email" required>
+                                             <label for="email">MS 365 Email</label>
                                              <div id="validationServerUsernameFeedback" class="invalid-feedback">
                                                   Please enter your MS 365 Email.
                                              </div>
                                         </div>
                                    </div>
                                    <div class="d-grid gap-2 md-3">
-                                        <button type="submit" name="registration_link" class="btn btn-primary text-light font-weight-bolder btn-lg">Send Registration Link</button>
+                                        <button type="submit" name="registration_link"
+                                             class="btn btn-primary text-light font-weight-bolder btn-lg">Send Registration Link</button>
+                                   </div>
+                                   <div class="text-end mb-3">
+                                        <p>
+                                             <a href="login" class="text-primary text-decoration-none fw-semibold">User Login</a>
+                                        </p>
                                    </div>
                               </form>
                          </div>
@@ -83,12 +93,6 @@ include('./admin/config/dbcon.php');
                </div>
           </div>
      </section>
-
-     <!-- Alertify JS link -->
-     <script src="assets/js/alertify.min.js"></script>
-
-     <!-- Custom JS link -->
-     <script src="assets/js/script.js"></script>
 
      <script>
           // Function to validate the form
@@ -112,12 +116,7 @@ include('./admin/config/dbcon.php');
                     });
           })();
      </script>
-
-     <?php
-     // Include scripts and message handling here
-     include('includes/script.php');
-     include('message.php'); 
-     ?>
+<?php include('includes/script.php'); ?>
 </body>
 
 </html>
