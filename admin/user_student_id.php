faccd14d29dff92b3102ee56557092afa969bbce
Black Mann <caranzocrisa@gmail.com>
2024-10-26 23:09:04 +0800

    commit

--- admin/user_student_id.php
+++ admin/user_student_id.php
@@ -212,7 +212,7 @@ if ($row) {
             </center> 
             <br>
             <div class="container" align="center">
-                <p style="font-size:13px;font-weight:bold;text-transform:capitalize;color:black;"><?php if(isset($names)){ $namez=$names;echo$namez;} ?></p>
+                <p style="font-size:14px;font-weight:bold;text-transform:capitalize;color:black;"><?php if(isset($names)){ $namez=$names;echo$namez;} ?></p>
                 <br>
             </div>
             <div>
