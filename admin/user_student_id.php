a08ba08774284fcd1a7fa03cb277c1da9c9f3f65
Black Mann <caranzocrisa@gmail.com>
2024-10-28 21:50:01 +0800

    commit

--- admin/user_student_id.php
+++ admin/user_student_id.php
@@ -234,7 +234,7 @@ if ($row) {
             <br>
                 <div class="container" align="center">
                     <p style="color:black;margin-top:-20px;">In case of emergency, please notify:</p>
-                    <p class="text-center" style="color:black;margin-top:20px;margin-bottom:-2px;font-size:14px;font-weight:bold;"><?php echo $contact_person; ?></p>
+                    <p class="text-center" style="color:black;margin-top:10px;margin-bottom:-2px;font-size:14px;font-weight:bold;"><?php echo $contact_person; ?></p>
                     <p class="text-center" style="color:black;font-size:14px;font-weight:bold;"><?php echo $person_cell_no; ?></p>
         </center>
                 </div>
