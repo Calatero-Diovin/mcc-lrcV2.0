58ae2eb9cfd3c79a60b5812e74a453d07644b44e
Black Mann <caranzocrisa@gmail.com>
2024-11-14 11:52:01 +0800

    commit

--- signup_script.php
+++ signup_script.php
@@ -12,29 +12,10 @@ if (!isset($_GET['code']) || empty($_GET['code'])) {
 
 $code = $_GET['code'];
 
-// Query to get the stored hash from the database
-$code_query = "SELECT username, verification_code FROM ms_account WHERE verification_code IS NOT NULL";
+$code_query = "SELECT username FROM ms_account WHERE verification_code = ?";
 $code_stmt = $con->prepare($code_query);
+$code_stmt->bind_param("s", $code);
 $code_stmt->execute();
 $code_result = $code_stmt->get_result();
-
-// Check if the code exists in the database
-$code_found = false;
-while ($code_row = $code_result->fetch_assoc()) {
-    // Verify the code with password_verify
-    if (password_verify($code, $code_row['verification_code'])) {
-        $code_found = true;
-        $username = $code_row['username'];
-        break;  // Exit loop as we found the matching code
-    }
-}
-
-$code_stmt->close();
-
-if (!$code_found) {
-    
-    header("HTTP/1.0 404 Not Found");
-    exit;
-}
-
+$code_row = $code_result->fetch_assoc();
 ?>
