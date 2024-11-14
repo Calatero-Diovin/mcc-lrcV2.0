2c534bf770c0c3ee0f654aef41e9d8a71767c1d6
Black Mann <caranzocrisa@gmail.com>
2024-11-14 21:40:09 +0800

    commit

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,14 @@
 <?php
+
+$request = $_SERVER['REQUEST_URI'];
+
+if (strpos($request, '.php') !== false) {
+    // Redirect to remove .php extension
+    $new_url = str_replace('.php', '', $request);
+    header("Location: $new_url", true, 301);
+    exit();
+}
+
 // In your header or a central initialization file
 if (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] !== 'on') {
      header("Location: https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
