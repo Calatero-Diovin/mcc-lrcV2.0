8781e8f023cb069978869d4af7b3c5f3ee09dd29
Black Mann <caranzocrisa@gmail.com>
2024-11-19 15:53:49 +0800

    commit

--- includes/navbar.php
+++ includes/navbar.php
@@ -2,7 +2,7 @@
 ini_set('session.cookie_httponly', 1);
 session_start(); 
 include('admin/config/dbcon.php');
-include('include/session.php');
+include('includes/session.php');
 ?>
 
 <nav class="navbar navbar-expand-lg" style="background: #0096FF;">
