09ae49cd4d6f17ea3b742369d2daecc18e392da6
Black Mann <caranzocrisa@gmail.com>
2024-11-22 14:23:54 +0800

    commit

--- includes/session.php
+++ includes/session.php
@@ -3,7 +3,7 @@
 setcookie('cookie_name', 'cookie_value', [
     'expires' => time() + 3600, // Cookie expiration time
     'path' => '/',
-    'domain' => 'example.com', // Replace with your domain
+    'domain' => 'mcc-lrc.com', // Replace with your domain
     'secure' => true,          // Ensure the cookie is sent over HTTPS
     'httponly' => true,        // Prevent access by JavaScript (for security)
     'samesite' => 'None'       // Allow cross-site cookies
