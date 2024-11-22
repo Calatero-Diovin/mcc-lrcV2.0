fc3fc11dc7bdde622bbe3bf60a57885f8ab00473
Black Mann <caranzocrisa@gmail.com>
2024-11-23 07:52:51 +0800

    commit

--- includes/security_headers.php
+++ includes/security_headers.php
@@ -4,4 +4,5 @@ header("X-XSS-Protection: 1; mode=block");
 header("X-Content-Type-Options: nosniff");
 header("Referrer-Policy: no-referrer");
 header("Strict-Transport-Security: max-age=31536000; includeSubDomains; preload");
+header("Content-Security-Policy: default-src 'self'; script-src 'self' https://mcc-lrc.com; style-src 'self'");
 ?>
