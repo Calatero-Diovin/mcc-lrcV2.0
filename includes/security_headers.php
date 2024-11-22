a777f1085b630a6690ebf38641fbb9df1d9203ee
Black Mann <caranzocrisa@gmail.com>
2024-11-23 07:47:46 +0800

    commit

--- includes/security_headers.php
+++ includes/security_headers.php
@@ -3,4 +3,5 @@ header("X-Frame-Options: DENY");
 header("X-XSS-Protection: 1; mode=block");
 header("X-Content-Type-Options: nosniff");
 header("Referrer-Policy: no-referrer");
+header("Strict-Transport-Security: max-age=31536000; includeSubDomains; preload");
 ?>
