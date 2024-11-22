ba372b6c455b4a62a4bce8020df19d7c9805aea4
Black Mann <caranzocrisa@gmail.com>
2024-11-22 13:15:25 +0800

    commit

--- includes/header.php
+++ includes/header.php
@@ -47,7 +47,7 @@ if (strpos($request, '.php') !== false) {
      <title>MCC Learning Resource Center</title>
 
      <!-- Bootstrap CSS -->
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+     <link rel="stylesheet" href="assets/css/bootstrap5.min.css" />
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.8.1/font/bootstrap-icons.min.css" />
      <link rel="stylesheet" href="assets/css/bootstrap-icons.min.css">
 
