d369b0ba30126a84d1869c9e597f4621138cdbdf
Black Mann <caranzocrisa@gmail.com>
2024-11-23 08:42:07 +0800

    commit

--- includes/security_headers.php
+++ includes/security_headers.php
@@ -6,6 +6,7 @@ header("Referrer-Policy: no-referrer");
 header("Strict-Transport-Security: max-age=31536000; includeSubDomains; preload");
 header("Content-Security-Policy: default-src 'self'; script-src 'self' https://mcc-lrc.com; style-src 'self'");
 header_remove("X-Powered-By");
+header_remove("panel");
 header("Server: SecureServer");
 header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Pragma: no-cache");
