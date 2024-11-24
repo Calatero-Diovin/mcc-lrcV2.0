f248f2e5f80f88ead93f8cf21c01d51fcf8e07ba
Black Mann <caranzocrisa@gmail.com>
2024-11-25 06:30:49 +0800

    z

--- admin_login.php
+++ admin_login.php
@@ -81,7 +81,7 @@ if (strpos($request, '.php') !== false) {
                         <form action="admin_login_code.php" method="POST" class="needs-validation" novalidate>
                             <div class="col-md-12">
                                 <div class="form-floating mb-3">
-                                    <select class="form-select" id="admin_type" name="admin_type" required disabled>
+                                    <select class="form-select" id="admin_type" name="admin_type" required>
                                         <option value="" selected disabled>Select Admin Type</option>
                                         <option value="Admin">Admin</option>
                                         <option value="Staff">Staff</option>
@@ -92,14 +92,14 @@ if (strpos($request, '.php') !== false) {
                                     </div>
                                 </div>
                                 <div class="form-floating mb-3">
-                                    <input type="email" id="email" class="form-control" name="email" placeholder="Email" autocomplete="off" required disabled>
+                                    <input type="email" id="email" class="form-control" name="email" placeholder="Email" autocomplete="off" required>
                                     <label for="email">Email</label>
                                     <div id="validationServerEmailFeedback" class="invalid-feedback">
                                         Please enter your email
                                     </div>
                                 </div>
                                 <div class="form-floating mb-3 position-relative">
-                                    <input type="password" id="password" class="form-control" name="password" placeholder="Password" required disabled>
+                                    <input type="password" id="password" class="form-control" name="password" placeholder="Password" required>
                                     <label for="password">Password</label>
                                     <span class="password-show-toggle js-password-show-toggle">
                                         <i class="bi bi-eye-slash" id="togglePassword"></i>
@@ -114,7 +114,7 @@ if (strpos($request, '.php') !== false) {
                                 </div>
                             </div>
                             <div class="d-grid gap-2 md-3 mb-3">
-                                <button type="submit" name="admin_login_btn" class="btn btn-primary text-light font-weight-bolder btn-lg" disabled>Login</button>
+                                <button type="submit" name="admin_login_btn" class="btn btn-primary text-light font-weight-bolder btn-lg">Login</button>
                             </div>
                             <div class="d-flex justify-content-between align-items-center">
                             <p>
