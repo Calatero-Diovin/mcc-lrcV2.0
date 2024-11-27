4d486c45a75213b56acc45ea66a1b0bb689e162c
Black Mann <caranzocrisa@gmail.com>
2024-11-27 23:49:41 +0800

    commit

--- allcode.php
+++ allcode.php
@@ -1,8 +1,62 @@
 <?php
-ini_set('session.cookie_httponly', 1);
 session_start();
 
 include('admin/config/dbcon.php');
+
+if (isset($_POST['password_update'])) {
+     $current_password = mysqli_real_escape_string($con, $_POST['current_password']);
+     $newpassword = mysqli_real_escape_string($con, $_POST['newpassword']);
+     $renewpassword = mysqli_real_escape_string($con, $_POST['renewpassword']);
+ 
+     // Validate the current password for user or faculty
+     $current_password_hashed = md5($current_password);
+     
+     $validate_user_query = "SELECT * FROM user WHERE password = '$current_password_hashed'";
+     $validate_faculty_query = "SELECT * FROM faculty WHERE password = '$current_password_hashed'";
+ 
+     $validate_user_query_run = mysqli_query($con, $validate_user_query);
+     $validate_faculty_query_run = mysqli_query($con, $validate_faculty_query);
+ 
+     if (!$validate_user_query_run || !$validate_faculty_query_run) {
+         die('Query failed: ' . mysqli_error($con));
+     }
+ 
+     if (mysqli_num_rows($validate_user_query_run) > 0 || mysqli_num_rows($validate_faculty_query_run) > 0) {
+         if ($newpassword == $renewpassword) {
+             $newpassword_hashed = md5($newpassword);
+             if (mysqli_num_rows($validate_user_query_run) > 0) {
+                 // Update password for user
+                 $change_pass_query = "UPDATE `user` SET password = '$newpassword_hashed' WHERE password = '$current_password_hashed'";
+             } else {
+                 // Update password for faculty
+                 $change_pass_query = "UPDATE `faculty` SET password = '$newpassword_hashed' WHERE password = '$current_password_hashed'";
+             }
+             $change_pass_run = mysqli_query($con, $change_pass_query);
+ 
+             if (!$change_pass_run) {
+                 $_SESSION['status'] = "Password not updated: " . mysqli_error($con);
+                 $_SESSION['status_code'] = "error";
+                 header("Location: myprofile.php");
+                 exit(0);
+             }
+ 
+             $_SESSION['status'] = "<small>Password updated successfully</small>";
+             $_SESSION['status_code'] = "success";
+             header("Location: myprofile.php");
+             exit(0);
+         } else {
+             $_SESSION['status'] = '<small>Password and confirm password do not match</small>';
+             $_SESSION['status_code'] = "warning";
+             header("Location: myprofile.php");
+             exit(0);
+         }
+     } else {
+         $_SESSION['status'] = 'Current password does not match';
+         $_SESSION['status_code'] = "warning";
+         header("Location: myprofile.php");
+         exit(0);
+     }
+ }
  
 
 if (isset($_SESSION['auth_stud']['stud_id'])) {
@@ -18,13 +72,14 @@ if (isset($_POST['save_changes'])) {
     $address = mysqli_real_escape_string($con, $_POST['address']);
     $phone = mysqli_real_escape_string($con, $_POST['phone']);
     $email = mysqli_real_escape_string($con, $_POST['email']);
-    $contact_person = mysqli_real_escape_string($con, $_POST['contact_person']);
-    $contact_person_cell = mysqli_real_escape_string($con, $_POST['contact_person_cell']);
 
+    // Update profile for user or faculty
     if (isset($_SESSION['auth_stud']['stud_id'])) {
-        $query = "UPDATE `user` SET firstname='$firstname', middlename='$middlename', lastname='$lastname', address='$address', cell_no='$phone', email='$email', contact_person='$contact_person', contact_person_cell='$contact_person_cell' WHERE user_id ='$id_session'";
+        // Update user profile
+        $query = "UPDATE `user` SET firstname='$firstname', middlename='$middlename', lastname='$lastname', address='$address', cell_no='$phone', email='$email' WHERE user_id ='$id_session'";
     } else {
-        $query = "UPDATE `faculty` SET firstname='$firstname', middlename='$middlename', lastname='$lastname', address='$address', cell_no='$phone', email='$email', contact_person='$contact_person', contact_person_cell='$contact_person_cell' WHERE faculty_id ='$id_session'";
+        // Update faculty profile
+        $query = "UPDATE `faculty` SET firstname='$firstname', middlename='$middlename', lastname='$lastname', address='$address', cell_no='$phone', email='$email' WHERE faculty_id ='$id_session'";
     }
     $query_run = mysqli_query($con, $query);
 
@@ -48,7 +103,7 @@ if (isset($_POST['logout_btn'])) {
     unset($_SESSION['auth_faculty']);
 
     $_SESSION['message_success'] = "Logout Successfully";
-    header("Location: .");
+    header("Location: home.php");
     exit(0);
 }
 ?>
