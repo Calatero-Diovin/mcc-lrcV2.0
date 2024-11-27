4719b392c41f2589dc7f73fb671f429757992aba
Black Mann <caranzocrisa@gmail.com>
2024-11-27 16:46:12 +0800

    a

--- admin/admin_login_code.php
+++ admin/admin_login_code.php
@@ -1,7 +1,7 @@
 <?php
 ini_set('session.cookie_httponly', 1);
 session_start();
-include('./admin/config/dbcon.php');
+include('onfig/dbcon.php');
 
 // Initialize session variables if not already set
 if (!isset($_SESSION['login_attempts'])) {
