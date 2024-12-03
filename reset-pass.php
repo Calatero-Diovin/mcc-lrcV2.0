0b65a6a796f873617c4189d3cba1c716ceecbf5d
Black Mann <caranzocrisa@gmail.com>
2024-12-04 07:18:46 +0800

    commit

--- reset-pass.php
+++ reset-pass.php
@@ -81,11 +81,11 @@ if (strpos($request, '.php') !== false) {
                               <div class="col-md-12">
                                 <a href="password-reset-otp.php" class="form-floating mb-3 p-3" style="text-decoration: none; border: 2px solid #ddd; border-radius: 20px; background-color: #f9f9f9; display: flex; align-items: center; justify-content: space-between; transition: background-color 0.3s;">
                                     <span><i class="bi bi-envelope" style="font-size: 2rem; color: #007bff;"></i></span>
-                                    <p class="reset-option" style="font-size: 1.2rem; color: #007bff; font-weight: bold; transition: color 0.3s;">Reset via Email OTP<br>Receive a code via email</button></p>
+                                    <p class="reset-option" style="font-size: 1.2rem; color: #007bff; font-weight: bold; transition: color 0.3s;">Reset via Email OTP<br><small>Receive a code via email</small></button></p>
                                 </a>
                                 <a href="password-reset.php" class="form-floating mb-3 p-3" style="text-decoration: none; border: 2px solid #ddd; border-radius: 20px; background-color: #f9f9f9; display: flex; align-items: center; justify-content: space-between; transition: background-color 0.3s;">
                                     <span><i class="bi bi-link-45deg" style="font-size: 2rem; color: #007bff;"></i></span>
-                                    <p class="reset-option" style="text-decoration: none; font-size: 1.2rem; color: #007bff; font-weight: bold; transition: color 0.3s;">Reset via Email Link<br>Receive a code via email</p>
+                                    <p class="reset-option" style="text-decoration: none; font-size: 1.2rem; color: #007bff; font-weight: bold; transition: color 0.3s;">Reset via Email Link<br><small>Receive a code via email</small></p>
                                 </a>
                               </div>
                                    <div class="text-end mt-5 fw-bold">
