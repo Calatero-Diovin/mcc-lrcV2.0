d8dec2eebe442952055ee8d9d9152c142b6aac1e
Black Mann <caranzocrisa@gmail.com>
2024-12-03 20:23:18 +0800

    commit

--- reset-pass.php
+++ reset-pass.php
@@ -75,7 +75,7 @@ if (strpos($request, '.php') !== false) {
                                             <span><i class="bi bi-envelope"></i></span>
                                              <a href="">Reset via OTP</a>
                                         </div>
-                                        <div class="form-floating mb-3">
+                                        <div class="form-floating mb-3 p-4">
                                             <span><i class="bi bi-link-45deg"></i></span>
                                              <a href="password-reset.php">Reset via Link</a>
                                         </div>
