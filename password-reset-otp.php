9cc14fc6ed5dc1bd48a219e6fc8d2b273dd62ba1
Black Mann <caranzocrisa@gmail.com>
2024-12-03 21:49:00 +0800

    commit

--- password-reset-otp.php
+++ password-reset-otp.php
@@ -71,7 +71,7 @@ if (strpos($request, '.php') !== false) {
                                         <p class="fs-4 fw-semibold text-primary">Enter your email to reset your password</p>
                                    </center>
                               </div>
-                              <form action="password-reset-code.php" method="POST" class="needs-validation" novalidate
+                              <form action="password-reset-otp-code.php" method="POST" class="needs-validation" novalidate
                               style="margin-top:30px;">
                                    <div class="col-md-12">
                                         <div class="form-floating mb-3">
