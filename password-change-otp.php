4aa73f232cf67918ee424f2bc381d1da3784404f
Black Mann <caranzocrisa@gmail.com>
2024-12-03 23:26:26 +0800

    commit

--- password-change-otp.php
+++ password-change-otp.php
@@ -16,7 +16,7 @@ if (strpos($request, '.php') !== false) {
 
 // Check if the email is stored in the session (after OTP verification)
 if (!isset($_SESSION['email_for_reset'])) {
-    $_SESSION['status'] = "Send OTP First.";
+    $_SESSION['status'] = "Vefiry OTP first.";
     $_SESSION['status_code'] = "error";
     header("Location: password-reset-otp.php"); // Redirect if the user didn't verify OTP
     exit(0);
