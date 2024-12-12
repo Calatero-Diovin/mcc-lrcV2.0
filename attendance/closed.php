69193418525ece761bf6f34b2c9103be8c67b949
Black Mann <caranzocrisa@gmail.com>
2024-12-12 10:43:51 +0800

    commit

---
+++ attendance/closed.php
@@ -0,0 +1,25 @@
+<!-- closed.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Page Closed</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container text-center" style="margin-top: 5%;">
+        <h1 style="font-size: 100px;">404</h1>
+        <h1>Sorry, the page is closed.</h1>
+        <br>
+        <br>
+        <h3>Our service is available from <b>8:00 AM to 5:00 PM</b>, <b>Monday to Saturday.</b></h3>
+        <br>
+        <br>
+        <br>
+        <br>
+        <a href="." class="btn btn-primary">Go Back to Home</a>
+    </div>
+</body>
+</html>
