c44aa1e774ebbf5b1695a3f96468f7655f367c31
Diovin Calatero <diovincalatero150@gmail.com>
2024-12-12 09:37:08 +0800

    a

--- attendance/index.php
+++ attendance/index.php
@@ -25,14 +25,14 @@ if (strpos($request, '.php') !== false) {
 <html lang="en">
 
 <head>
-<meta charset="UTF-8" />
+     <meta charset="UTF-8" />
      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
      <meta name="robots" content="noindex, nofollow" />
-     <link rel="icon" href="./images/mcc-lrc.png">
+     <link rel="icon" href="../images/mcc-lrc.png">
      <title>MCC Learning Resource Center</title>
      <link href="https://fonts.gstatic.com" rel="preconnect" />
-     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i"
           rel="stylesheet" />
      <!-- Bootstrap CSS -->
      <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
@@ -49,27 +49,22 @@ if (strpos($request, '.php') !== false) {
      <!-- Alertify JS link -->
      <link rel="stylesheet" href="assets/css/alertify.min.css" />
      <link rel="stylesheet" href="assets/css/alertify.bootstraptheme.min.css" />
-     
      <!-- Datatables -->
      <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-     <link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.dataTables.css">
-     <link rel="stylesheet" href="https://cdn.datatables.net/rowreorder/1.5.0/css/rowReorder.dataTables.css">
-     <link rel="stylesheet" href="https://cdn.datatables.net/responsive/3.0.3/css/responsive.dataTables.css">
+     <link rel="stylesheet" href="assets/css/dataTables.bootstrap5.min.css">
+
+     <link rel="stylesheet" type="text/css"
+          href="https://cdn.datatables.net/1.13.1/css/dataTables.bootstrap5.min.css" />
+     <link rel="stylesheet" type="text/css"
+          href="https://cdn.datatables.net/buttons/2.3.3/css/buttons.bootstrap5.min.css" />
 
      <!-- Custom CSS -->
      <link href="assets/css/style.css" rel="stylesheet" />
-     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.0/dist/sweetalert2.min.css">
-     <link href="assets/css/sweetalert2.min.css" rel="stylesheet" />
 
      <!-- Animation -->
      <link rel="stylesheet" href="https://www.cssportal.com/css-loader-generator/" />
      <!-- Loader -->
      <link rel="stylesheet" href="https://www.cssportal.com/css-loader-generator/" />
-
-     <link rel="stylesheet" href="assets/css/bootstrap-datepicker.min.css">
-
-     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
-     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.js"></script>
 
      <script>
         function updateClock() {
@@ -299,15 +294,12 @@ if (strpos($request, '.php') !== false) {
 
 
 
-     <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
-<script src="https://cdn.datatables.net/2.1.8/js/dataTables.js"></script>
-<script src="https://cdn.datatables.net/buttons/3.1.2/js/dataTables.buttons.js"></script>
-<script src="https://cdn.datatables.net/buttons/3.1.2/js/buttons.dataTables.js"></script>
-<script src="https://cdn.datatables.net/buttons/3.1.2/js/buttons.print.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/3.1.2/js/buttons.html5.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
+     <!-- <script src="assets/js/bootstrap.bundle.min.js"></script> -->
+     <!-- <script src="assets/js/jquery-3.6.0.min.js"></script> -->
+     <script src="assets/js/datatables.min.js"></script>
+     <script src="assets/js/pdfmake.min.js"></script>
+     <script src="assets/js/vfs_fonts.js"></script>
+     <script src="assets/js/custom.js"></script>
 
 
 
