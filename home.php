3a9133bbca294705adf1a4e4c538d3abe3285c46
Black Mann <caranzocrisa@gmail.com>
2024-12-15 21:21:58 +0800

    a

--- home.php
+++ home.php
@@ -69,7 +69,7 @@ include('includes/navbar.php');
         /* Chat messages */
         .chatbox-messages {
             flex: 1;
-            padding: 10px;
+            padding: 5px;
             overflow-y: auto;
             border-top: 1px solid #ddd;
         }
