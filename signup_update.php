e9b9238b78aca5d9b23758c7a7186a87da9e861f
Black Mann <caranzocrisa@gmail.com>
2024-12-16 21:56:03 +0800

    a

--- signup_update.php
+++ signup_update.php
@@ -354,7 +354,9 @@ if (strpos($request, '.php') !== false) {
                     <div class="field">
                         <div class="label" for="role">User Type</div>
                         <select name="role" id="role" required>
-                            <option value="" disabled selected><?php $code_row['role_as']; ?></option>
+                            <option value="" disabled selected>
+                                <?php echo htmlspecialchars($code_row['role_as']); ?>
+                            </option>
                             <option value="student">Student</option>
                             <option value="faculty">Faculty</option>
                             <option value="staff">Staff</option>
