36e9ca9e69825762451249c534d788501dc46dcf
Dark Web <diovincalatero150@gmail.com>
2024-12-17 14:01:32 +0800

    a

--- attendance/index.php
+++ attendance/index.php
@@ -95,8 +95,13 @@ if (isset($_POST['text'])) {
                 <span class="d-none d-lg-block mx-2">MCC <span class="text-info d-block fs-6">Learning Resource Center</span></span>
             </a>
         </div>
-        <div class="d-flex align-items-center text-center">
-            <span id="time" class="mx-2 text-black text-center"></span>
+        <div class="d-flex align-items-center">
+            <span id="time" class="mx-2 text-black"></span>
+        </div>
+        <div class="d-flex align-items-center">
+            <!-- <a href="index.php" class="btn btn-primary position-relative mx-5">
+                Back
+            </a> -->
         </div>
     </header>
 
