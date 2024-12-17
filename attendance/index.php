61a483726dd9ff4d5428ea0e26cb62d6204fdd72
Dark Web <diovincalatero150@gmail.com>
2024-12-17 14:07:20 +0800

    a

--- attendance/index.php
+++ attendance/index.php
@@ -109,7 +109,7 @@ if (isset($_POST['text'])) {
         <section class="section dashboard">
             <div class="container">
                 <div class="row">
-                    <div class="col-md-6 d-flex justify-content-center align-items-center" style="height: 100vh;">
+                    <div class="col-md-6 d-flex justify-content-center align-items-center">
                         <video id="preview" width="100%" style="max-width: 100%; max-height: 100%;"></video>
                     </div>
                     <div class="col-md-6">
