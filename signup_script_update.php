69d8aa9770d0b7f470d7d53708d8dd98b1b671e7
Dark Web <diovincalatero150@gmail.com>
2024-12-18 09:47:59 +0800

    a

--- signup_script_update.php
+++ signup_script_update.php
@@ -27,11 +27,11 @@ $faculty_result = $faculty_stmt->get_result();
 
 if ($code_result->num_rows > 0) {
     $code_row = $code_result->fetch_assoc();
-    // $created_at = new DateTime($code_row['created_at']); // Creation time of the verification code
+    // $user_added = new DateTime($code_row['user_added']); // Creation time of the verification code
     // $current_time = new DateTime(); // Current time
 
     // // Check if the difference is greater than 1 hour
-    // $created_at_timestamp = $created_at->getTimestamp();
+    // $created_at_timestamp = $user_added->getTimestamp();
     // $current_time_timestamp = $current_time->getTimestamp();
 
     // if (($current_time_timestamp - $created_at_timestamp) > 3600) { // 3600 seconds = 1 hour
@@ -40,6 +40,17 @@ if ($code_result->num_rows > 0) {
     // }
 } elseif ($faculty_result->num_rows > 0) {
     $faculty_row = $faculty_result->fetch_assoc();
+    // $faculty_added = new DateTime($faculty_row['faculty_added']); // Creation time of the verification code
+    // $current_time = new DateTime(); // Current time
+
+    // // Check if the difference is greater than 1 hour
+    // $created_at_timestamp = $faculty_added->getTimestamp();
+    // $current_time_timestamp = $current_time->getTimestamp();
+
+    // if (($current_time_timestamp - $created_at_timestamp) > 3600) { // 3600 seconds = 1 hour
+    //     header("Location: 404.php");
+    //     exit;
+    // }
 } else {
     header("Location: 404.php");
     exit;
