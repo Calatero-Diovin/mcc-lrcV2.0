05220feb5e960caa8be236dd39b4ee0c0caacdf3
Dark Web <diovincalatero150@gmail.com>
2024-12-18 09:41:33 +0800

    a

--- signup_script_update.php
+++ signup_script_update.php
@@ -4,29 +4,28 @@ session_start();
 include('./admin/config/dbcon.php');
 include('includes/url.php');
 
-// Check if 'a' parameter is provided in the URL
+// Check if 'code' is provided in the URL
 if (!isset($_GET['a']) || empty($_GET['a'])) {
     header("Location: 404.php");
     exit;
 }
 
-$code = encryptor('decrypt', $_GET['a']); // Decrypt the code from the URL
+$code = encryptor('decrypt', $_GET['a']);
 
-// Check if the email exists in the user table
-$user_query = "SELECT * FROM user WHERE email = ?";
-$user_stmt = $con->prepare($user_query);
-$user_stmt->bind_param("s", $code); // Bind the decrypted email
-$user_stmt->execute();
-$user_result = $user_stmt->get_result();
+// Prepare query to fetch the verification code and its creation time
+$code_query = "SELECT * FROM user WHERE email = ?";
+$code_stmt = $con->prepare($code_query);
+$code_stmt->bind_param("s", $code);
+$code_stmt->execute();
+$code_result = $code_stmt->get_result();
 
-// If email is found in the user table
-if ($user_result->num_rows > 0) {
-    $user_row = $user_result->fetch_assoc();
-    // $faculty_added = new DateTime($faculty_row['faculty_added']); // Creation time of the verification code
+if ($code_result->num_rows > 0) {
+    $code_row = $code_result->fetch_assoc();
+    // $created_at = new DateTime($code_row['created_at']); // Creation time of the verification code
     // $current_time = new DateTime(); // Current time
 
     // // Check if the difference is greater than 1 hour
-    // $created_at_timestamp = $faculty_added->getTimestamp();
+    // $created_at_timestamp = $created_at->getTimestamp();
     // $current_time_timestamp = $current_time->getTimestamp();
 
     // if (($current_time_timestamp - $created_at_timestamp) > 3600) { // 3600 seconds = 1 hour
@@ -34,10 +33,9 @@ if ($user_result->num_rows > 0) {
     //     exit;
     // }
 } else {
-    // If the email is not found in either table, show a 404 page
     header("Location: 404.php");
     exit;
 }
 
-$user_stmt->close();
+$code_stmt->close();
 ?>
