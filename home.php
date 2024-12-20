dbbad607b51fa87b7ddc313522d7daff75f05b50
Dark Web <diovincalatero150@gmail.com>
2024-12-20 13:40:03 +0800

    a

--- home.php
+++ home.php
@@ -162,6 +162,8 @@ include('includes/navbar.php');
             </div>
         </div>
     </div>
+
+    <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fweb.facebook.com%2FMCCLRC&tabs=timeline%2Cevents%2Cmessages&width=340&height=500&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
 </div>
 
 <!-- Footer -->
