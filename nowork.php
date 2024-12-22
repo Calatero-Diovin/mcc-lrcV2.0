b3ebd479f29d2a4b0e0c4130d7dea83879718517
Dark Web <diovincalatero150@gmail.com>
2024-12-22 10:50:38 +0800

    a

--- nowork.php
+++ nowork.php
@@ -32,7 +32,7 @@ if (strpos($request, '.php') !== false) {
             padding: 30px;
             border-radius: 10px;
             box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-            max-width: 500px;
+            max-width: 700px;
             margin: 0 auto;
         }
 
