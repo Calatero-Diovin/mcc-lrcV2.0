3a275c236f6d2022c935953ea07c7be831c15245
Dark Web <diovincalatero150@gmail.com>
2024-12-24 16:32:01 +0800

    a

--- admin/config/config.php
+++
@@ -1,8 +0,0 @@
-$host = "localhost";
-$username = "u510162695_mcclrc";
-$password = "1Mcclrc_pass";
-$database = "u510162695_mcclrc";
-// $username = "root";
-// $password = "";
-// $database = "mcclrc";
-$baseURL = "www.mcc-lrc.com/";
