51ff12dec0e7de09efedd3c1bf574c469fa68d28
Dark Web <diovincalatero150@gmail.com>
2024-12-29 10:35:52 +0800

    a

--- admin/ms_account.php
+++ admin/ms_account.php
@@ -43,8 +43,6 @@ include('./includes/sidebar.php');
                                                                            <th>Firstname</th>
                                                                            <th>Lastname</th>
                                                                            <th>Email</th>
-                                                                           <th>Used</th>
-                                                                           <th>Action</th>
                                                                       </tr>
                                                                  </thead>
                                                                  <tbody>
@@ -58,15 +56,11 @@ include('./includes/sidebar.php');
                                                                                      <td>{$row['firstname']}</td>
                                                                                      <td>{$row['lastname']}</td>
                                                                                      <td>{$row['username']}</td>
-                                                                                     <td>{$row['used']}</td>
-                                                                                     <td>
-                                                                                         <button class='btn btn-sm btn-primary edit-btn' data-username='{$row['username']}' data-used='{$row['used']}'>Edit</button>
-                                                                                     </td>
                                                                                 </tr>";
                                                                            }
                                                                            $stmt->close();
                                                                       } else {
-                                                                           echo "<tr><td colspan='5'>Error retrieving data</td></tr>";
+                                                                           echo "<tr><td colspan='4'>Error retrieving data</td></tr>";
                                                                       }
                                                                       ?>
                                                                  </tbody>
@@ -84,31 +78,6 @@ include('./includes/sidebar.php');
      </section>
 </main>
 
-<!-- Edit Modal -->
-<div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
-     <div class="modal-dialog">
-          <div class="modal-content">
-               <div class="modal-header">
-                    <h5 class="modal-title" id="editModalLabel">Edit Used Value</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-               </div>
-               <form action="update_used.php" method="POST">
-                    <div class="modal-body">
-                         <input type="hidden" name="username" id="edit-username">
-                         <div class="mb-3">
-                              <label for="edit-used" class="form-label">Used</label>
-                              <input type="number" class="form-control" id="edit-used" name="used" required>
-                         </div>
-                    </div>
-                    <div class="modal-footer">
-                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                         <button type="submit" class="btn btn-primary">Save Changes</button>
-                    </div>
-               </form>
-          </div>
-     </div>
-</div>
-
 <?php 
 include('./includes/footer.php');
 include('./includes/script.php');
@@ -120,21 +89,10 @@ include('../message.php');
 <script>
 document.addEventListener('DOMContentLoaded', function () {
      new DataTable('#example', {
-          responsive: true,
-          rowReorder: {
-               selector: 'td:nth-child(2)'
-          }
-     });
-
-     // Handle Edit Button Click
-     document.querySelectorAll('.edit-btn').forEach(button => {
-          button.addEventListener('click', function () {
-               const username = this.dataset.username;
-               const used = this.dataset.used;
-               document.getElementById('edit-username').value = username;
-               document.getElementById('edit-used').value = used;
-               new bootstrap.Modal(document.getElementById('editModal')).show();
-          });
-     });
+    responsive: true,
+    rowReorder: {
+        selector: 'td:nth-child(2)'
+    }
+});
 });
 </script>
