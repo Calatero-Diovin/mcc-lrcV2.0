96a1994d56dcaab80e6f0595a55e6e6297386aa0
Dark Web <diovincalatero150@gmail.com>
2024-12-29 09:50:50 +0800

    a

--- admin/user_student.php
+++ admin/user_student.php
@@ -52,7 +52,7 @@ include('./includes/sidebar.php');
                               <table id="example" class="display nowrap" style="width:100%">
                                         <thead>
                                              <tr>
-                                                  <th style="display:none;"><center></center></th>
+                                                  <th style="visibilty:hidden;"><center></center></th>
                                                   <th><center>Full Name</center></th>
                                                   <th><center>Image</center></th>
                                                   <th><center>Student No</center></th>
